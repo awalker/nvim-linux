9f096570657e8e2151b694dd713629538cc7c4be
Adam Walker <adamwalker@graylion.dev>
2023-03-03 10:08:09 -0500

    set clipboard for windows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- only set this on windows...
+vim.cmd('set clipboard = "unnamedplus"')
+
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.scrolloff = 8
