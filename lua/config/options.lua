517881f56adc4e21e115e50badd2551d906e239b
Adam Walker <adam.walker@virtualmindset.com>
2023-04-24 11:12:04 -0400

    wip: folding

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,8 +3,13 @@
 -- Add any additional options here
 
 -- only set this on windows...
--- vim.cmd('set clipboard = "unnamedplus"')
+vim.cmd('set clipboard += "unnamedplus"')
 
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.scrolloff = 8
+-- vim.opt.title = true
+
+-- Treesitter folding
+vim.wo.foldmethod = "expr"
+--vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
