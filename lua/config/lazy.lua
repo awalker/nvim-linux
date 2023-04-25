fdc7039a454cb2bc5ea691fe6bef6050531226aa
Adam Walker <adam.walker@virtualmindset.com>
2023-04-25 10:46:47 -0400

    chore: current

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,11 +14,14 @@ require("lazy").setup({
       import = "lazyvim.plugins",
       -- TODO: Use onedark on windows
       -- colorscheme = "whatever"
+      opts = {
+        colorscheme = "catppuccin-mocha",
+      },
     },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -31,8 +34,8 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = false }, -- automatically check for plugin updates
+  install = { colorscheme = { "tokgnight", "habamax", "catppuccin" } },
+  checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    opts = {
+      -- transparent = true,
+      style = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+  },
+}
